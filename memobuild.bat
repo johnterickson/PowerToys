3a9a525cc8ce4a064bef24dc5055ef019838096d
John Erickson <jerick@microsoft.com>
2023-10-03 11:12:07 -0700

    try to use packagereferences

--- memobuild.bat
+++ memobuild.bat
@@ -12,7 +12,6 @@ rmdir /S /Q %USERPROFILE%\.nuget\packages\memobuild
 git submodule update --init --recursive
 git clean -xdf
 curl.exe https://dist.nuget.org/win-x86-commandline/v6.7.0/nuget.exe -o tools\nuget.exe
-tools\nuget restore packages.config -SolutionDirectory . || exit /b 1
 tools\nuget restore .\PowerToys.sln || exit /b 1
 IF "%SYSTEM_TEAMFOUNDATIONCOLLECTIONURI%"=="https://dev.azure.com/artifactsandbox0/" (
     for /F %%I in ('az account get-access-token --query accessToken --output tsv') DO (set "SYSTEM_ACCESSTOKEN=%%I")
